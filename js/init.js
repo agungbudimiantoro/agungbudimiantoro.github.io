131a2d80de088f0dab9324f9803e07986c73569f
agungbudimiantoro <agung.miantoro@gmail.com>
2020-06-01 15:47:32 +0700

    ubah data

--- js/init.js
+++ js/init.js
@@ -6,25 +6,3 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.parallax').parallax();
 });
-
-
-$(document).ready(function () {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 30,
-    nav: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    pagination: true,
-    responsive: {
-      0: {
-        items: 2,
-        margin: 10
-      },
-      800: {
-        items: 4
-      }
-    }
-  });
-});
